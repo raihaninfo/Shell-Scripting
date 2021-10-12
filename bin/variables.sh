b3c226c7821c0f0e5be4a7b5b8c517acf130f432
raihaninfo <raihanmahmudi35@gmail.com>
2021-10-12 14:32:14 +0600

    prin differentt variable in different line and write some notes

--- bin/variables.sh
+++ bin/variables.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# The name of a variable can only contain lerrers (a to z or A to Z), Number (0 to 9), and the underscore character (_)
+
+# Furthermore, a variable's name can only Start with a letter or an underscore.
+
+# Don't include space in a variable name.
+
 x=3
 y=34
 z=23
@@ -8,5 +14,9 @@ z=23
 echo "$x"
 echo '$y'
 echo $z
+
+echo "The value of x is $x"
+# line breack
+echo "The value ox y is $y \nThe value of z is $z"
 
 # pwd
