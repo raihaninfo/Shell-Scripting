a9b0ef7e1b73fb151eee7406d62b10539d604c3d
raihaninfo <raihanmahmudi35@gmail.com>
2021-10-16 06:24:04 +0600

    make loop in infinity

--- bin/guess_my_number.sh
+++ bin/guess_my_number.sh
@@ -1,22 +1,23 @@
 # !/bin/bash
 
-for each in 1 2 3
-do
-    system_number=$RANDOM
+system_number=$RANDOM
+no_chances=1
+for (( ; ; ))
+ do
 
-    read -p "This is your chance no $each , Guess My Number : " number
+    read -p "This is your chance no $no_chances , Guess My Number : " number
 
-    if [ $number -eq $system_number ]
-    then
+    if [ $number -eq $system_number ]; then
         echo "Your Guess is currect"
+        break
     else
         echo "Your Guess is wrong"
-        if [ $number -ge $system_number ]
-        then 
+        if [ $number -ge $system_number ]; then
             echo "Please Select some lower value than $number"
-        else 
+        else
             echo "Please Select some heigher value than $number"
         fi
     fi
+    no_chances=$((no_chances + 1))
 done
 echo "your are fail"
