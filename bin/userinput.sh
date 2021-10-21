a72ed9dbb24a1043b0eeee1fa2fd85ec67ff3ac2
raihaninfo <raihanmahmudi35@gmail.com>
2021-10-21 14:59:17 +0600

    rename variable.md to variable_and_userinput, and create new file

---
+++ bin/userinput.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#####################################
+# Author: RAihan
+# Version: v1.0.0
+# Date: 21/10/2021
+# Description: Our first script using variables!
+# Usage: ./hello-world-variable.sh
+#####################################
+
+name=${1}
+
+echo "Hello $name"
