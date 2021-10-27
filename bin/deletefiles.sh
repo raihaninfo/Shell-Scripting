e4fb05171ae1d7b75ab99710fdeea72d24bd4ac9
raihaninfo <raihanmahmudi35@gmail.com>
2021-10-27 22:36:31 +0600

    created file a bash script delet any user files

---
+++ bin/deletefiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+user=${1}
+path=${2}
+function delete() {
+    # FILE_PATH=
+    if [ -f ${2} ]; then
+        sudo -u ${1} rm -r ${2}
+        echo "File removed"
+    
+    else
+        echo "This file is not available"
+    fi
+}
+
+delete "$user $path"
