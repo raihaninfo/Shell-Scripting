eee1689c12e25795d8a2f8a4ceca315eef46fdd6
raihaninfo <raihanmahmudi35@gmail.com>
2021-10-30 18:05:43 +0600

    code format

--- bin/grep-then-else.sh
+++ bin/grep-then-else.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
-
 FILE_NAME=/tmp/grep-then-else.txt
+
 # Touch the file; creates it if it does not exist.
 touch ${FILE_NAME}
 # Check the file for the keyword.
@@ -11,7 +10,7 @@ grep_rc=$?
 # If the file contains the keyword, remove the file. Otherwise, write
 # the keyword to the file.
 if [[ ${grep_rc} -eq 0 ]]; then
-rm ${FILE_NAME}
+    rm ${FILE_NAME}
 else
-echo 'keyword' >> ${FILE_NAME}
+    echo 'keyword' >>${FILE_NAME}
 fi
